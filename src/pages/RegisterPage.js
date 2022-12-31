985b65b16b8c539d5dcc881fefb04901b405ae2e
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2022-12-31 15:57:13 +0900

    feat 회원가입 1차 디자인

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -3,8 +3,19 @@ import '../style/css/registerPage.css';
 
 const RegisterPage = () => {
   return (
-    <div>
-      <div className='testBox'></div>
+    <div className='register_background'>
+      <div className='register_title'>
+        <h1>🌱SaessakChat🌱</h1>
+      </div>
+      <div className='register_inputContainer'>
+        <div className='register_IDBox'>
+          <input className='register_inputID'></input>
+          <button className='register_idChk'>중복확인</button>
+        </div>
+        <div className='register_warningMSG'>
+          4~12자의 영문 대소문자와 숫자로만 입력해주세요.
+        </div>
+      </div>
     </div>
   )
 }
