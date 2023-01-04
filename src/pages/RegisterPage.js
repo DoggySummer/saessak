fc6dc068fb1a9e1a5b12ee27b3c0d40614b2273d
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2023-01-04 15:47:50 +0900

    feat component제작

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Register_input from '../Components/registerPage/Register_input';
 import '../style/css/registerPage.css';
 
 const RegisterPage = () => {
@@ -15,35 +16,22 @@ const RegisterPage = () => {
         <div className='register_warningMSG'>
           4~12자의 영문 대소문자와 숫자로만 입력해주세요.
         </div>
-        <div className='register_IDBox'>
-          <input className='register_inputForm'></input>
-        </div>
-        <div className='register_warningMSG'>
-          8~16자의 소문자,숫자,특수문자를 사용하세요.
-        </div>
-        <div className='register_IDBox'>
-          <input className='register_inputForm'></input>
-        </div>
-        <div className='register_warningMSG'>
-          비밀번호가 일치하지 않습니다.
-        </div>
-        <div className='register_IDBox'>
-          <input className='register_inputForm'></input>
-        </div>
-        <div className='register_warningMSG'>
-          올바른 정보를 입력해주세요
-        </div>
-        <div className='register_IDBox'>
-          <input className='register_inputForm'></input>
-        </div>
-        <div className='register_warningMSG'>
-          이메일 형식이 맞지 않습니다.
-        </div>
+        <Register_input message={"8~16자의 소문자,숫자,특수문자를 사용하세요." } />
+        <Register_input message={"비밀번호가 일치하지 않습니다." } />
+        <Register_input message={"올바른 정보를 입력해주세요." } />
+        <Register_input message={"이메일 형식이 맞지 않습니다." } />
         <div className='register_btn'>
           회원가입
         </div>
         <div className='login_routeBtn'>
           로그인 화면으로 돌아가기
+        </div>
+        <div className='idchk_complete'>
+          이 아이디는 사용가능한 아이디입니다.
+            <div className='idButtonBox'>
+              <div className='usingID'>아이디 사용</div>
+              <div className='cancelID'>취소</div>
+            </div>
         </div>
       </div>
     </div>
